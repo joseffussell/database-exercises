35447dda3c4447ddb280170bc5c1aa31a28f0af4
Josef Fussell <josef.k.fussell@gmail.com>
2023-01-30 09:06:29 -0600

    quicksave

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,1 +1,8 @@
 USE codeup_test_db;
+
+SELECT name AS 'All Pink Floyd Albums' FROM albums WHERE artist = 'Pink Floyd';
+SELECT release_date AS 'Year album was released' FROM albums WHERE name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+SELECT genre AS 'Genre for Nevermind' FROM albums WHERE name = 'Nevermind';
+SELECT name AS 'Albums released in 1990s' FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+SELECT name AS 'Albums with less than 20 mil sales' FROM albums WHERE sales BETWEEN 0 AND 20;
+SELECT name AS 'Albums labelled as Rock' FROM albums WHERE genre = 'Rock';
